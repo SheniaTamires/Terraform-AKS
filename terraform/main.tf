536c89237efc3df54b058255e00a6ff546f71559
Shenia Tamires Andrade Bezerra <74924125+SheniaTamires@users.noreply.github.com>
2024-08-05 21:08:59 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-resource "azurerm_resource_group2" "rg" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_cluster_name
-  location            = azurerm_resource_group2.rg.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "k8s"
 
