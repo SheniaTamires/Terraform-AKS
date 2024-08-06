56a62e8c0ecd222d4ee3d9df9720c2a6dabbcc37
Shenia Tamires Andrade Bezerra <74924125+SheniaTamires@users.noreply.github.com>
2024-08-05 21:34:10 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "agentpool"
     node_count = var.node_count
-    vm_size    = "Standard_LRS"
+    vm_size    = "standard_b16pls_v2"
   }
 
   identity {
